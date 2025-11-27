c5d29fa42acaa7f308e4b548f0b9efe1089e31ad
Nadia Mallah <n.mallah2004@gmail.com>
2025-11-27 09:34:49 +0100

    Initialisation: Projet Spring Boot de base

---
+++ src/main/java/com/example/todo/TodoApiCollaboratifApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiCollaboratifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiCollaboratifApplication.class, args);
+	}
+
+}
